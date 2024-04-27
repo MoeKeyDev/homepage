b439f0b5c5192b153e0ecda77c5cf67de1a4d8f1
noDream <1802024110@qq.com>
2024-04-27 16:42:21 +0800

    Add homepage components and styles to optimize the interface display and interaction

---
+++ docs/.vuepress/components/HomePage.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="box">
+    <a class="download" href="https://jsdelivr.007666.xyz/gh/ChenDoXiu/MoeKey@main/docs/MoeKey.exe" download="MoeKey.exe" >下载</a>
+  </div>
+</template>
+
+<style scoped>
+.box{
+  height: 100vh;
+  width: 100vw;
+  min-width: 800px; /* 你想要的最小宽度 */
+  background-image: url("https://jsdelivr.007666.xyz/gh/ChenDoXiu/MoeKey@main/docs/banner.png");
+  background-repeat: no-repeat;
+  background-size: cover; /* 或者 contain */
+}
+.download{
+  position: absolute;
+  bottom: 158px;
+  left: 410px;
+  transform: translateX(-50%);
+  padding: 10px 20px;
+  border-radius: 5px;
+  background-color: #fff;
+  color: #000;
+  text-decoration: none;
+  font-size: 18px;
+  font-weight: bold;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'MoeKey',
+    description: 'A Misskey client for Flutter',
+    head: [
+        ['link', { rel: 'icon', href: 'https://jsdelivr.007666.xyz/gh/ChenDoXiu/MoeKey@main/assets/favicon.png' }]
+    ],
+    themeConfig: {
+        logo: 'https://jsdelivr.007666.xyz/gh/ChenDoXiu/MoeKey@main/assets/favicon.png',
+        search: false,
+        nav: [
+            { text: 'GitHub', link: 'https://github.com/ChenDoXiu/MoeKey' }
+        ]
+    }
+}
